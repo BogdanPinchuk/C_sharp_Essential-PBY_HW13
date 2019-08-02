5d251a1580a52056975547da83f2681d10406017
Bogdan <pinchuk.brus@gmail.com>
2019-08-02 15:49:04 +0300

    Скореговано падіння слів

---
+++ LesApp1/Counter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp1
+{
+    /// <summary>
+    /// Циклічний лічильник
+    /// </summary>
+    class Counter
+    {
+        private int iteration = -1;
+
+        /// <summary>
+        /// Номер ітерації
+        /// </summary>
+        public int Iteration
+        {
+            get
+            {
+                if (iteration == MaxSize)
+                {
+                    iteration = 0;
+                }
+                else
+                {
+                    iteration++;
+                }
+                return iteration;
+            }
+            private set { iteration = value; }
+        }
+        /// <summary>
+        /// Максимальна величиниа лічильника
+        /// </summary>
+        public int MaxSize { get; private set; }
+
+        /// <summary>
+        /// конструктор який встановлює мінімальний розмір
+        /// </summary>
+        /// <param name="maxSize">максимальний розмір лічильника</param>
+        public Counter(int maxSize)
+        {
+            MaxSize = maxSize;
+        }
+
+        /// <summary>
+        /// конструктор який становлює мінімальний розмір і початок ітератора
+        /// </summary>
+        /// <param name="maxSize">максимальний розмір лічильника</param>
+        /// <param name="iteration">звідки починає стартувати, але потім ни заходить нижче нуля</param>
+        public Counter(int maxSize, int iteration)
+        {
+            MaxSize = maxSize;
+            Iteration = iteration;
+
+            if (iteration == 0)
+            {
+                Color = ConsoleColor.White;
+            }
+            else if (iteration == -1)
+            {
+                Color = ConsoleColor.Green;
+            }
+            else
+            {
+                Color = ConsoleColor.DarkGreen;
+            }
+        }
+
+        /// <summary>
+        /// Колір букви
+        /// </summary>
+        public ConsoleColor Color { get; set; }
+    }
+}
--- LesApp1/Program.cs
+++ LesApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LesApp1
@@ -11,11 +12,11 @@ namespace LesApp1
         /// <summary>
         /// Кількість рядків
         /// </summary>
-        private static int row = Console.LargestWindowHeight;
+        private static int rowM = Console.WindowHeight;
         /// <summary>
         /// Кількість колонок
         /// </summary>
-        private static int col = Console.LargestWindowWidth;
+        private static int colM = Console.WindowWidth;
         /// <summary>
         /// Набір символів для виведення (рандомно)
         /// </summary>
@@ -32,7 +33,11 @@ namespace LesApp1
         /// <summary>
         /// Випадкові значення
         /// </summary>
-        Random rnd = new Random();
+        private Random rnd = new Random();
+        /// <summary>
+        /// Блокування консолі
+        /// </summary>
+        private static object block = new object();
 
         static void Main()
         {
@@ -42,12 +47,26 @@ namespace LesApp1
             // Join Unicode
             Console.OutputEncoding = Encoding.Unicode;
 
+            #region testing
+            /*
+            Counter count = new Counter(10, 0);
 
+            while (true)
+            {
+                Console.WriteLine(count.Iteration);
+                Thread.Sleep(50);
+            }
+            */
+            #endregion
 
-
+            for (int i = 0; i < colM; i++)
+            {
+                new Thread(() => RainWords(arrayI[1], i)).Start();
+                Thread.Sleep(500);
+            }
 
             // Repeat
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         /// <summary>
@@ -57,7 +76,49 @@ namespace LesApp1
         /// <param name="col">номер колонки падіння</param>
         private static void RainWords(string word, int col)
         {
+            // створення лічильника для кожної букви слова
+            Counter[] counter = new Counter[word.Length + 1];
 
+            // налаштування лічильників слова
+            for (int i = 0; i < counter.Length; i++)
+            {
+                // установка початкових значень лічильника (-1 для рядків, бо уходить за межі і губиться одна буква)
+                counter[i] = new Counter(rowM - 1, -i);
+                // Останній для закреслення
+            }
+
+            // безкінечний цикл, пускаємо по кругу слово згори в низ
+            while (true)
+            {
+                // пробіжка по всьому слову
+                for (int i = 0; i < word.Length + 1; i++)
+                {
+                    // блокування доступу до виводу в консоль
+                    lock (block)
+                    {
+                        int j = counter[i].Iteration;
+                        // запис символа пвного кольору у відповідній позиції
+                        if (j >= 0)
+                        {
+                            Console.ForegroundColor = counter[i].Color;
+                            Console.SetCursorPosition(col, j);
+
+                            // перевірка довжини слова, якщо іретація більша довжини 
+                            // то пишемо пробіл
+                            if (i < word.Length)
+                            {
+                                Console.Write(word[i]);
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+
+                    }
+                }
+                Thread.Sleep(50);
+            }
         }
 
 
